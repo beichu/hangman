2096f6152e5e6a69012387afed41e6a248189455
Bei Chu <sproutstofruition@gmail.com>
2021-10-28 23:31:51 -0400

    Add hint button and css for tablet-size screens.

--- app/main.py
+++ app/main.py
@@ -1,145 +1,172 @@
 import math
-from browser import document, bind, window, ajax
-from browser.widgets.dialog import InfoDialog
-
-
-
+from browser import document, bind, window, ajax, html
+from browser.widgets.dialog import Dialog
+import json
 
 
 class Panel:
 
     def __init__(self, canvas):
         self.ctx = canvas.getContext("2d")
-        self.center_x = canvas.width / 2
-        self.center_y = canvas.height / 2
-        self.score = 0
-        self.guesses = 10
-        self.head_radius = 0.1
-        self.body_length = 0.2
-        self.arm_length = 0.2
-        self.leg_length = 0.2
-        self.gallows_height = 0.8
+        self.ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     def draw_gallows(self, canvas):
-        self.ctx.clearRect(0, 0, canvas.width, canvas.height)
         ctx = self.ctx
         ctx.beginPath()
-        ctx.moveTo(self.center_x - canvas.width/4, canvas.height * 0.99)
-        ctx.lineTo(self.center_x + canvas.width/4, canvas.height * 0.99)
-        ctx.moveTo(self.center_x , canvas.height * 0.99)
-        ctx.lineTo(self.center_x , self.gallows_height)
-        ctx.lineTo(self.center_x + self.head_radius*canvas.height * 2, self.gallows_height)
-        ctx.lineTo(self.center_x + self.head_radius*canvas.height * 2, self.center_y - self.head_radius*canvas.height)
+        ctx.moveTo(canvas.width*0.25, canvas.height)
+        ctx.lineTo(canvas.width*0.75, canvas.height)
+        ctx.moveTo(canvas.width*0.5, canvas.height)
+        ctx.lineTo(canvas.width*0.5, 0)
+        ctx.lineTo(canvas.width*0.6, 0)
+        ctx.lineTo(canvas.width*0.6, canvas.height*0.25)
         ctx.stroke()
-        ctx.closePath()
+
     
-    def draw_hangman(self, counter):
+    def draw_hangman(self, canvas, counter):
         ctx = self.ctx
         ctx.beginPath()
         if counter == 1: # draw head
-            ctx.arc(self.center_x + self.head_radius*canvas.height * 2, self.center_y, self.head_radius*canvas.height, 0, math.pi * 2)
+            ctx.arc(canvas.width*0.6, 
+                    canvas.height*0.35, 
+                    0.1 * canvas.height, 
+                    0, math.pi * 2)
             ctx.stroke()
         if counter == 2: # draw body
-            ctx.moveTo(self.center_x+ self.head_radius*canvas.height * 2, self.center_y + self.head_radius*canvas.height)
-            ctx.lineTo(self.center_x+ self.head_radius*canvas.height * 2, self.center_y + self.head_radius*canvas.height + self.body_length*canvas.height)
+            ctx.moveTo(canvas.width*0.6, 
+                       canvas.height*0.45)
+            ctx.lineTo(canvas.width*0.6, 
+                       canvas.height*0.65)
             ctx.stroke()
         if counter == 3: # draw left arm
-            ctx.moveTo(self.center_x+ self.head_radius*canvas.height * 2, self.center_y + self.head_radius*canvas.height + 0.2*self.body_length*canvas.height)
-            ctx.lineTo(self.center_x+ self.head_radius*canvas.height * 2 - self.arm_length*canvas.height/math.sqrt(2), self.center_y + self.head_radius*canvas.height + self.arm_length*canvas.height/math.sqrt(2))
+            ctx.moveTo(canvas.width*0.6, 
+                       canvas.height*0.55)
+            ctx.lineTo(canvas.width*0.6-canvas.height*0.14, 
+                       canvas.height*0.41)
             ctx.stroke()
         if counter == 4: # draw right arm
-            ctx.moveTo(self.center_x+ self.head_radius*canvas.height * 2, self.center_y + self.head_radius*canvas.height+ 0.2*self.body_length*canvas.height)
-            ctx.lineTo(self.center_x+ self.head_radius*canvas.height * 2 + self.arm_length*canvas.height/math.sqrt(2), self.center_y + self.head_radius*canvas.height + self.arm_length*canvas.height/math.sqrt(2))
+            ctx.moveTo(canvas.width*0.6, 
+                       canvas.height*0.55)
+            ctx.lineTo(canvas.width*0.6+canvas.height*0.14, 
+                       canvas.height*0.41)
             ctx.stroke()
         if counter == 5: # draw left leg
-            ctx.moveTo(self.center_x+ self.head_radius*canvas.height * 2, self.center_y + self.head_radius*canvas.height + self.body_length*canvas.height)
-            ctx.lineTo(self.center_x+ self.head_radius*canvas.height * 2 - self.leg_length*canvas.height/math.sqrt(2), self.center_y + self.head_radius*canvas.height + self.body_length*canvas.height + self.leg_length*canvas.height/math.sqrt(2))
+            ctx.moveTo(canvas.width*0.6, 
+                       canvas.height*0.65)
+            ctx.lineTo(canvas.width*0.6-canvas.height*0.14, 
+                       canvas.height*0.8)
             ctx.stroke()        
         if counter == 6: # draw right leg
-            ctx.moveTo(self.center_x+ self.head_radius*canvas.height * 2, self.center_y + self.head_radius*canvas.height + self.body_length*canvas.height)
-            ctx.lineTo(self.center_x+ self.head_radius*canvas.height * 2 - self.leg_length*canvas.height/math.sqrt(2), self.center_y + self.head_radius*canvas.height + self.body_length*canvas.height + self.leg_length*canvas.height/math.sqrt(2))
+            ctx.moveTo(canvas.width*0.6, 
+                       canvas.height*0.65)
+            ctx.lineTo(canvas.width*0.6+canvas.height*0.14, 
+                       canvas.height*0.8)
             ctx.stroke()
             
  
+class Game:
+
+    def __init__(self):
+        "initialize the game"
+        self.canvas = document["my_canvas"]       
+        self.panel = Panel(self.canvas)
+        self.panel.draw_gallows(self.canvas)
+        
+        self.message_div = document["message"]
+        self.guessed_div = document["guessed_letters"]
+        self.message_div.text = ""
+        self.guessed_div.text = ""
+        self.top_div = document["top_word"]
+        
+        self.guessed_set = set()
+
+        self.counter = 0
+        self.word = self.get_word()
+        
+        document["input"].bind("keypress", self.check)      
+        document["hint"].bind("click", self.get_def)
+
     
-def get_word():
-    "get a random word from the random word API"
-    ajax.get("https://random-word-api.herokuapp.com/word?number=1",
-              timeout=5,
-              cache = True,
-             oncomplete=show_word)
+    def get_def(self, ev):
+        "get word def from the free dictionary api"
+        print('here')
+        url = "https://api.dictionaryapi.dev/api/v2/entries/en/{word}".format(word = self.word)
+        ajax.get(url, timeout = 15, cache = True, oncomplete=self.show_hint)
+
+    
+    def get_word(self):
+        "get a random word from the random word API"
+        ajax.get("https://random-word-api.herokuapp.com/word?number=1",
+                  timeout=5,
+                  cache = True,
+                 oncomplete=self.show_word)
+    
 
 
-def show_word(req):
-    global word 
-    global top
-    word = req.text[2:-2]
-    display = "_ "*len(word)
-    top.html = display
-    
 
-def new_game():
-    global guessed_set
-    global counter
-    global panel
-    global guessed_text
-    global word 
-    global top
-    global canvas
-    
-    word = ""
-    
-    guessed_text = document["guessed_letters"]
-    guessed_text.text = ""
-    guessed_set = set()
-    get_word()
-    counter = 0
-    
-    canvas = document["myCanvas"]
-    panel = Panel(canvas)
-    panel.draw_gallows(canvas)
-    document["input"].bind("keypress", check)
-    top = document["top"]
+    def show_word(self, req):
+        "display the word as a series of underscores"
+        self.word = req.text[2:-2]
+        self.top_div.html = "_ "*len(self.word)
 
-# the entry field has the id "guess"
-def check(ev):
-    global word
-    global guessed_set
-    global guessed_text
-    global counter
-    global panel
-    global top
     
-    word_set = set([letter for letter in word])
-    if ev.keyCode == 13:
-        input_field = document["input"]
-        message = document["message"]
-        if input_field.value.lower() in "abcdefghijklmnopqrstuvwxyz":
-            message.text = ""
-            char = input_field.value.lower()
-            if char not in guessed_set:
-                guessed_set.add(char)
-                if len(guessed_set) == 1:
-                    guessed_text.text += char
-                else:
-                    guessed_text.text += (', ' +char) 
-                if char not in word:
-                    counter += 1
-                    panel.draw_hangman(counter)
-                    if counter == 6:
-                        message = "You Lose!"
-                        InfoDialog("", message, ok="Play again?")
-                        new_game()
-                        top.html = word
-                else:
-                    display = "".join(["_ " if letter not in guessed_set else letter for letter in word])
-                    top.html = display
-                    if counter < 6 and "_" not in display:
-                        InfoDialog("", "You win!", ok="Play again?")
+    def show_hint(self, req):
+        print(req.text)
+        try:
+            if "No Definitions Found" in req.text:
+                word_def = "Sorry, no hint available."
             else:
-                message.text = 'You already guessed the letter "' + char + '"!'
-                    
+                word_def = str(json.loads(req.text[1:-1])['meanings'][0]['definitions'][0]['definition'])
+            self.message_div.text = 'Hint: ' + word_def
+        except:
+            pass
+    
+    def show_dialogue(self, status):
+        d = Dialog('', default_css=False, ok_cancel=True)
+        @bind(d.ok_button, "click")
+        def ok(ev):
+            "Event handler for ok button"
+            self.__init__()
+            document["input"].unbind("keypress", self.check)
+            d.close()
+                
+        
+        if status == 'lose':
+            d.panel <= html.DIV("You Lose!<br>Play Again?")
         else:
-            message.text = "Please type a letter that's in the English alphabet!"
-        input_field.value = ""  # reset input filed value after last input
+            d.panel <= html.DIV("You Win!<br>Play Again?")
 
+
+
+    def check(self, ev):
+        input_field = document["input"]
+        if ev.keyCode == 13: # 13 is the code for the "Enter" key    
+            letter = input_field.value.lower() # convert to lower case where possible
+            if (not (letter.isalpha()) or (len(letter) != 1)): 
+                self.message_div.text = "Please type a valid English letter!"
+            else: # input is valid
+                self.message_div.text = ""
+                if letter not in self.guessed_set:
+                    self.guessed_set.add(letter)
+                    if len(self.guessed_set) == 1:
+                        self.guessed_div.text = letter
+                    else:
+                        self.guessed_div.text += (', ' + letter)
+                    if letter not in self.word:
+                        self.counter += 1
+                        self.panel.draw_hangman(self.canvas, self.counter)
+                        if self.counter == 6:
+                            self.top_div.html = self.word
+                            self.show_dialogue('lose')  
+                    else: # letter in word
+                        display = "".join(["_ " if letter not in self.guessed_set else letter for letter in self.word]) 
+                        self.top_div.html = display
+                        if self.counter < 6 and "_" not in display:
+                            self.show_dialogue('win')
+                
+                else:
+                    self.message_div.text = 'You already guessed the letter "' + letter + '"!'    
+            
+            input_field.value = ""  # reset input filed value after last input
+
+
+game = Game()
